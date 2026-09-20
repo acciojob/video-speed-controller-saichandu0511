eb027e4647207c8d04c78e96180801176698531f
saichandu0511 <saichandanreddy226@gmail.com>
2026-09-20 11:33:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+let video = document.querySelector(".flex");
+let speed = document.querySelector(".speed");
+let speedBar = document.querySelector(".speed-bar");
+
+function changeSpeed(e) {
+
+    let y = e.pageY - speed.offsetTop;
+
+    let percent = y / speed.offsetHeight;
+
+    let min = 0.4;
+    let max = 4;
+
+    let playbackRate = percent * (max - min) + min;
+
+    speedBar.style.height = percent * 100 + "%";
+
+    speedBar.textContent = playbackRate.toFixed(1) + "×";
+
+    video.playbackRate = playbackRate;
+}
+
+speed.addEventListener("mousemove", changeSpeed);
